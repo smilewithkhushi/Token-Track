48c4453a256510e9f79089d508a3a6908782c54c
smilewithkhushi <khushipanwar26@gmail.com>
2024-03-24 21:01:55 +0530

    [fixed] provider

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import {
 
 function App() {
   const { ethers, JsonRpcProvider } = require("ethers");
+  const { utils } = require("ethers");
 
   //State variables
   const [tokens, setTokens] = useState([]);
@@ -32,6 +33,7 @@ function App() {
     e.preventDefault();
     console.log("address = ", address);
     setAddress(address);
+
     fetchTokens()
       .then((data) => {
         setTokens(data.assets);
@@ -45,44 +47,29 @@ function App() {
 
   //Function to fetch tokens
   const fetchTokens = async () => {
-    // if (!ethers.isAddress(address)) {
-    //   alert("Please enter a valid Ethereum wallet address");
-    //   return;
-    // }
+    if (!utils.isAddress(address)) {
+      alert("Please enter a valid Ethereum wallet address");
+      return;
+    }
 
-    const provider = new JsonRpcProvider(
-     process.env.QUICKNODE_RPC_URL
-      );
+    //provider created for etherjs version 5.4
+    const provider = new ethers.providers.JsonRpcProvider(
+      process.env.QUICKNODE_RPC_URL
+    );
 
-    const response = await provider.send("qn_getWalletTokenBalance", [
-      {
-        wallet: address,
-        // contracts: []
+    console.log("provider created");
 
-        contracts: [
-          "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", //WETH
-          "0xdAC17F958D2ee523a2206206994597C13D831ec7", // USDT
-          "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0", // MATIC
-          "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", // USDC
-          "0x6B175474E89094C44Da98b954EedeAC495271d0F", // DAI
-          "0xb24cd494faE4C180A89975F1328Eab2a7D5d8f11", // CODE
-          "0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72", // ENS
-          "0xB8c77482e45F1F44dE1745F52C74426C631bDD52", // BNB
-          "0x4Fabb145d64652a948d72533023f6E7A623C7C53", // BUSD
-          "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84", // stETH
-          "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE", // SHIB
-          "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984", // UNI
-          "0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39", // HEX
-          "0x514910771AF9Ca656af840dff83E8264EcF986CA", // LINK
-          "0x4d224452801ACEd8B2F0aebE155379bb5D594381", // APE
-          "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32", // LDO
-          "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9", // AAVE
-          "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2", // MKR
-        ],
-      },
-    ]);
-    // console.log(response.result);
-    return response.result;
+    const tokens = await provider.send("qn_getWalletTokenBalance", {
+      wallet: address,
+      contracts: [
+        '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', //WETH
+        '0xdAC17F958D2ee523a2206206994597C13D831ec7', //USDT
+        '0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0', //MATIC
+        '0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72', //ENS
+      ]
+    });
+    console.log("tokens = ", tokens);
+    return tokens;
   };
 
   return (
